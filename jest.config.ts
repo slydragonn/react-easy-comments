ac8a8b3ac792cba854e500ed3bbfcefc92786c1f
slydragonn <alogo18m@gmail.com>
2022-09-06 16:24:19 -0500

    feat(jest): add esm support

--- jest.config.ts
+++ jest.config.ts
@@ -65,15 +65,23 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+
+  globals: {
+    "NODE_ENV": "test",
+    'ts-jest': {
+      useESM: true,
+    }
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: [
+    "node_modules",
+    "/src",
+  ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -88,7 +96,10 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "ts-jest",
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -175,19 +186,19 @@ export default {
   // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest'
-  }
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
+  transformIgnorePatterns: [
+   "<rootDir>/node_modules/(?!dateformat)",
   //   "\\.pnp\\.[^\\\\]+$"
-  // ],
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
